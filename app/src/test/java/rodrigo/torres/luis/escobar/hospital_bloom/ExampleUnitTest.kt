c9978ec8d644135e94bf2c07956f10c60f4dc940
Thomas <20230441@ricaldone.edu.sv>
2024-07-15 14:55:11 -0600

    creacion del proyecto

---
+++ app/src/test/java/rodrigo/torres/luis/escobar/hospital_bloom/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package rodrigo.torres.luis.escobar.hospital_bloom
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
